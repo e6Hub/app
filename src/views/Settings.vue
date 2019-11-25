4671f6af618cea00ad152ed670aa2094b51f2afc
Saektide <real.saektide@gmail.com>
2019-11-25 10:00:10 -0300

    +appver to settings view

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,6 +1,10 @@
 <template>
-    <div id="settings-container" class="inline-flex w-full p-6">
+    <div id="settings-container" class="inline-block w-full p-6">
         <h2 class="text-2xl font-bold uppercase text-gray-600">Settings</h2>
+        <div id="app-version">
+            <h3 class="text-xl font-bold uppercase text-gray-500">App version</h3>
+            <p>{{require('../../package.json').version}}</p>
+        </div>
     </div>
 </template>
 
