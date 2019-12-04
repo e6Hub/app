568d8559cba5ed84be952efded2e06f00b413d01
Saektide <real.saektide@gmail.com>
2019-12-04 20:36:35 -0300

    adding linux to build

--- vue.config.js
+++ vue.config.js
@@ -3,24 +3,31 @@ module.exports = {
         electronBuilder: {
             nodeModulesPath: ['../../node_modules', './node_modules'],
             builderOptions: {
-                appId: "saektide.esix-hub.app",
-                productName: "e6Hub",
+                appId: 'saektide.esix-hub.app',
+                productName: 'e6Hub',
+                // Windows
                 win: {
-                    target: ["nsis"],
-                    icon: "./e6h.ico"
+                    target: ['nsis'],
+                    icon: './e6h.ico'
                 },
                 nsis: {
-                    installerLanguages: "en_US",
-                    installerIcon: "./e6h_ins.ico",
-                    uninstallerIcon: "./e6h_unins.ico",
+                    installerLanguages: 'en_US',
+                    installerIcon: './e6h_ins.ico',
+                    uninstallerIcon: './e6h_unins.ico',
                     oneClick: false,
                     runAfterFinish: false,
                     allowToChangeInstallationDirectory: true
                 },
+                // Linux
+                linux: {
+                    target: ['deb'],
+                    synopsis: 'A hub for furries',
+                    category: 'Network'
+                },
                 publish: {
-                    provider: "github",
-                    owner: "e6Hub",
-                    repo: "app"
+                    provider: 'github',
+                    owner: 'e6Hub',
+                    repo: 'app'
                 }
             }
         }
