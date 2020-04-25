609c790f2243318e2f5653417e156a660ecc642c
Saektide <real.saektide@gmail.com>
2020-04-24 23:25:54 -0300

    adding PurgeCSS

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,6 +1,12 @@
 module.exports = {
 	plugins: [
-	  require("tailwindcss"),
-	  require("autoprefixer")(),
+		require('tailwindcss'),
+		require('autoprefixer')(),
+		process.env.NODE_ENV === 'production' ? purgecss({
+		content: [
+				'./src/**/*.html', 
+				'./src/**/*.vue'
+			]
+		}): ''
 	]
 };
