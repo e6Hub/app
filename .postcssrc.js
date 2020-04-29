a922562ab06b2e4c3af5d09a51d2f76677f7fc0d
Saektide <real.saektide@gmail.com>
2020-04-28 22:37:06 -0300

    purgecss is kinda weird for this type of project

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,14 +1,6 @@
-const purgecss = require("@fullhuman/postcss-purgecss");
-
 module.exports = {
 	plugins: [
 		require('tailwindcss'),
-		require('autoprefixer')(),
-		process.env.NODE_ENV === 'production' ? purgecss({
-		content: [
-				'./src/**/*.html', 
-				'./src/**/*.vue'
-			]
-		}): ''
+		require('autoprefixer')()
 	]
 };
